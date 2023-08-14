d3c9c69e94653f446f21e216f07295f1ff6d14c6
Leandro <leandrodev83@gmail.com>
2023-08-14 14:21:57 -0300

    Atualizand a view dashboard, com o controller DashboardController

--- crud-livro/routes/web.php
+++ crud-livro/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::prefix('/dashboard')->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
